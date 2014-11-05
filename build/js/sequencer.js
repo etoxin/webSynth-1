1b63d71bbe0861cba00516aadf19573508e10198
etoxin <adam@etoxin.net>
2014-11-05 23:08:47 +1100

    stuff

--- build/js/sequencer.js
+++ build/js/sequencer.js
@@ -4,7 +4,7 @@
  * Created by adamlusted on 3/11/14.
  */
 var angular = require('angular');
-}).call(this,require("1YiZ5S"),typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {},require("buffer").Buffer,arguments[3],arguments[4],arguments[5],arguments[6],"/fake_b59dfa68.js","/")
+}).call(this,require("1YiZ5S"),typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {},require("buffer").Buffer,arguments[3],arguments[4],arguments[5],arguments[6],"/fake_c5c31f1f.js","/")
 },{"1YiZ5S":6,"angular":2,"buffer":3}],2:[function(require,module,exports){
 (function (process,global,Buffer,__argument0,__argument1,__argument2,__argument3,__filename,__dirname){
 /**
